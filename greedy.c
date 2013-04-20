4915347cd8249be35be58e9a233aac371615ac38
Jessie Hawkins <jessie.hawkins1@gmail.com>
2013-04-19 22:46:38 -0400

    first commit

--- greedy.c
+++ greedy.c
@@ -1,54 +1,65 @@
+/**********************************************************************
+*
+* Jessie Hawkins - pset1 - greedy.c
+*
+* This program asks the user for amount of change due and then returns the
+* smallest amount of coins that can be used to distribute the change.
+*
+***********************************************************************/
+
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
 
 int main(void)
 {
-   // ask for user input of change
-   float user_input;
-   do
-   {
-       printf("Hi friend, how much change do you have?\n");
-       user_input = GetFloat();
-   }
-   while (user_input < 0.0);
+    // ask for change from user input
+    float user_input;
+    do
+    {
+        printf("Hi friend, how much change do you have?\n");
+        user_input = GetFloat();
+    }
+    while (user_input < 0.0);
 
-   //convert float to int
-   int change = (round)(user_input * 100);
-   
-   //declare counter
-   int coin = 0;  
+    //convert float to int
+    int change = (round)(user_input * 100);
 
-   //check for quarters
-   while (change >= 25) 
-   {
+    //declare counter
+    int coin = 0;
+
+    //if cents owed is >= to a quarter, subtract quarter and add 1 to coin counter
+    while (change >= 25)
+    {
         coin++;
         change = change - 25;
-   } 
+    }
 
-   //check for dimes
-   while (change >= 10)
-   {
+    //if cents owed is >= to a dime, subtract dime and add 1 to coin counter
+    while (change >= 10)
+    {
         coin++;
         change = change - 10;
-   }
+    }
 
-   //check for nickels
-   while (change >= 5)
-   {
+    //if cents owed is >= to a nickel, subtract nickel and add 1 to coin counter
+    while (change >= 5)
+    {
         coin++;
         change = change - 5;
-   }
+    }
 
-   //check for pennies
-   while (change >= 1)
-   {
+    //if cents owed is >= to a penny, subtract penny and add 1 to coin counter
+    while (change >= 1)
+    {
         coin++;
         change = change - 1;
-   }
+    }
 
-   //print number of coins
-   printf("%d\n", coin);
-   return 0;
+    //print number of coins
+    printf("%d\n", coin);
+
+    //return 0 to indicate no errors occured
+    return 0;
 }
-   
+
--- mario.c
+++ mario.c
@@ -1,31 +1,46 @@
+/******************************************************************
+*
+* Jessie Hawkins- pset1 - mario.c
+*
+* This program asks the user for the height of a pyramid
+* and then generates the pyramid using spaces and hashes.
+*
+*******************************************************************/
+
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    // get height from user
+    // get height of pyramid from user input
     int height;
     do
     {
         printf("Height: ");
         height = GetInt();
     }
-    while (height < 0 || height > 23);    
+    while (height < 0 || height > 23);
 
     // print rows
     for (int row = 1; row <= height; row++)
     {
         // print spaces
-        for (int space = (height-row); space >= 1; space--)
+        for (int space = (height - row); space >= 1; space--)
+        {
             printf(" ");
+        }
 
         // print hashes
-        for (int hash = (height-row) ; hash <= height ; hash++)
-            printf("#");  
+        for (int hash = (height - row) ; hash <= height ; hash++)
+        {
+            printf("#");
+        }
 
-	//print new line
-        printf("\n");           
-    }    
-    return 0;      
+        //print new line
+        printf("\n");
+    }
+
+    //return 0 to indicate no errors occured
+    return 0;
 }
-    
+
